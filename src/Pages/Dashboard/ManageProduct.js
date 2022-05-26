248afcdeec87d68f5b8bad8b68c11c641f120050
HasanAhamed1 <hasanahmed95695@gmail.com>
2022-05-26 13:11:39 +0600

    manage item for admin

--- src/Pages/Dashboard/ManageProduct.js
+++ src/Pages/Dashboard/ManageProduct.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading';
+import ProductRow from './ProductRow';
 
-const ManageProduct = () => {
+
+const ManageProducts = () => {
+    const {data: products, isLoading, refetch} = useQuery('products', () => fetch('http://localhost:5000/tools', {
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res=>res.json()));
+
+    if(isLoading){
+        return <Loading></Loading>
+    }
     return (
         <div>
-            
+            <h2 className="text-2xl">Manage Products: {products.length}</h2>
+            <div class="overflow-x-auto">
+  <table class="table w-full">
+    
+    <thead>
+      <tr>
+        <th></th>
+        <th></th>
+        <th>Id</th>
+        <th>Product Name</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+        {
+            products.map((product, index) => <ProductRow
+             key={product._id}
+             product={product}
+             index={index}
+             refetch={refetch}
+             ></ProductRow>)
+        }
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
 
-export default ManageProduct;
+export default ManageProducts;
